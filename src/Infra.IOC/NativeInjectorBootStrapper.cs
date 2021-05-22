bf773632344620f6c8cbd92b19bb57cd50014279
Bruno Crema Ferreira <eng.brn@gmail.com>
2021-05-22 01:38:46 -0300

    Blackboard Pattern Implementation

--- src/Infra.IOC/NativeInjectorBootStrapper.cs
+++ src/Infra.IOC/NativeInjectorBootStrapper.cs
@@ -24,8 +24,6 @@ namespace LiloDash.Infra.IOC
                 .RegisterMediator()
                 .RegisterServiceBroker()
 
-                
-            
                 //Application Services
                 .AddScoped<IBuildingAppService, BuildingAppService>()
             
