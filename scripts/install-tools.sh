5963934f5468a9f0cbf1cf8a3957abec943135a3
Bruno Crema Ferreira <eng.brn@gmail.com>
2021-06-19 17:05:45 -0300

    Script adjusts

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,66 +1,81 @@
 #!/bin/bash
 
-#Text Decorations
-RED='\033[1;31m'
-YLL='\033[1;33m'
-GRE='\033[1;32m'
-NC='\033[0m'
+. ./lib.sh
+log "DOT NET DEVELOPMENT ENVIRONMENT" intro 1.0.0
 
-echo "${YLL}+----------------|Tools Setup - Lilo DashBoard|----------------+${NC}"
+#------------------|Dependencies Check|------------------
 
-#-----------------------|Dependencies Check|------------------------------------------------------------
-echo "${YLL}Checking Dependencies...${NC}"
+#Checking APT
+log "Checking dependencies" title
+checkDependency apt APT
+if [ $? -eq 1 ]; then
+    die
+fi   
+#------------------|APT Update        |------------------
 
-#Check APT
-if [ -x "$(command -v apt)" ]; then
-    echo "${GRE}[X] APT is installed...${NC}"
-else
-    echo "${RED}[ ] APT is not installed...${NC}"
-    echo ""
-    echo "${RED}Script aborted.${NC}"
-    exit
-fi
+#Running Apt Update
+log "Updating Source List" title
+sudo apt-get update
 
-#-----------------------|Installing Dotnet|--------------------------------------------
-#Check DotNet V 5.7 - Ubuntu V 21.04+
-if [ -x "$(command -v dotnet)" ]; then
-    echo "${GRE}[X] DotNet 5 is installed...${NC}"
-else
-    echo "${RED}[ ] DotNet 5 is not installed...${NC}"
-    echo "${GRE}Installing...${NC}"
+
+log "Installer" title
+#------------------|Installing .Net 5  |------------------
+
+checkDependency dotnet ".NET Core 5"
+if [ $? -eq 1 ]; then
+    log "Installing..." success
     wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update
     sudo apt-get install -y apt-transport-https && \
     sudo apt-get update && \
     sudo apt-get install -y dotnet-sdk-5.0
-    rm packages-microsoft-prod.deb
+    rm -f packages-microsoft-prod.deb
 fi
 
-#-----------------------|Installing Vs Code|--------------------------------------------
-#Check Vs Code
-if [ -x "$(command -v code)" ]; then
-    echo "${GRE}[X] Vs Code is installed...${NC}"
-else
-    echo "${RED}[ ] Vs Code is not installed...${NC}"
-    echo "${GRE}Installing...${NC}"
-    wget https://az764295.vo.msecnd.net/stable/b4c1bd0a9b03c749ea011b06c6d2676c8091a70c/code_1.57.0-1623259737_amd64.deb
-    sudo dpkg -i code_1.57.0-1623259737_amd64.deb
-    sudo apt-get -f install
-    rm -f code_1.57.0-1623259737_amd64.deb
-fi
+#-----------------|Installing Entity Framework|------------
 
-#-----------------------|Installing Entity Framework|--------------------------------------------
-#Check EF
-if [ -x "$(command -v dotnet-ef)" ]; then
-    echo "${GRE}[X] EF is installed...${NC}"
-else
-    echo "${RED}[ ] EF is not installed...${NC}"
-    echo "${GRE}Installing...${NC}"
+checkDependency dotnet-ef "Entity Framework"
+if [ $? -eq 1 ]; then
+    log "Installing..." success
     dotnet tool install --global dotnet-ef
 fi
 
-#-----------------------|End|---------------------------------------------------------------------------
-echo ""
-echo "${GRE}Script Finalized.${NC}"
-echo "${YLL}+-------------------------------------------------------------------------------------+${NC}"
+#------------------|VS Code            |------------------
+
+checkDependency code "Visual Studio Code"
+if [ $? -eq 1 ]; then
+    log "Installing..." success
+    wget https://az764295.vo.msecnd.net/stable/b4c1bd0a9b03c749ea011b06c6d2676c8091a70c/code_1.57.0-1623259737_amd64.deb
+    sudo dpkg -i code_1.57.0-1623259737_amd64.deb
+    rm -f code_1.57.0-1623259737_amd64.deb
+fi
+
+#------------------|VS Code - Extensions|------------------
+log "Visual Code Extensions" title
+installVsExtension()
+{
+    local extensionName="$1"
+    log "Installing: $extensionName ..." success
+    code --install-extension $extensionName
+}
+
+installVsExtension "ms-dotnettools.csharp"
+installVsExtension "alefragnani.project-manager"
+installVsExtension "vscode-icons-team.vscode-icons"
+installVsExtension "jchannon.csharpextensions"
+installVsExtension "coenraads.bracket-pair-colorizer"
+installVsExtension "christian-kohler.path-intellisense"
+installVsExtension "jmrog.vscode-nuget-package-manager"
+installVsExtension "alefragnani.bookmarks"
+installVsExtension "eamodio.gitlens"
+installVsExtension "donjayamanne.githistory"
+installVsExtension "felipecaputo.git-project-manager"
+installVsExtension "github.vscode-pull-request-github"
+installVsExtension "spywhere.guides"
+installVsExtension "abusaidm.html-snippets"
+installVsExtension "ms-azuretools.vscode-docker"
+installVsExtension "fernandoescolar.vscode-solution-explorer"
+
+#------------------|End of Script|------------------
+log "End of Script..." success
---
+++ scripts/lib.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+#Text Decorations
+RED='\033[1;31m'
+YLL='\033[1;33m'
+GRE='\033[1;32m'
+NC='\033[0m'
+
+# Script
+printLogo()
+{
+    echo "${YLL}"
+    cat << "EOF"
+
+ _      _ _         _____            _     
+| |    (_) |       |  __ \          | |    
+| |     _| | ___   | |  | | __ _ ___| |__          __
+| |    | | |/ _ \  | |  | |/ _` / __| '_ \    (___()'`;
+| |____| | | (_) | | |__| | (_| \__ \ | | |   /,    /`
+|______|_|_|\___/  |_____/ \__,_|___/_| |_|   \\"--\\ 
+                                            
+                                                               
+EOF
+echo "${NC}"
+}
+
+#
+# Purpose: Display Message Log
+#
+log()
+{
+    local mode=$2
+    local message="$1"
+    case $mode in
+        intro)
+            
+            printLogo
+            echo "${YLL}    Script: ${NC}${GRE}$message${NC}"
+            echo "${YLL}    Version: ${NC}${GRE}$3${NC}"
+            echo "${YLL}+------------------------------------------------------------------------------+${NC}"
+            ;;
+        title)
+            echo "${YLL}+----------------|$message|----------------+${NC}"
+            ;;
+        success)
+            echo "${GRE}$message${NC}"
+            break
+            ;;
+        information)
+            echo "${YLL}$message${NC}"
+            break
+            ;;
+        warning)
+            echo "${YLL}$message${NC}"
+            break
+            ;;
+        error)
+            echo "${RED}$message${NC}"
+            break
+            ;;
+        *)
+            echo "$message"
+            ;;
+    esac
+}
+
+#
+# Purpose: Display message and die with given exit code
+# 
+die(){
+        local message="$1"
+        local exitCode="$2"
+        
+        echo ""
+        log "$message" error
+        log "Script aborted." warning
+        echo ""
+        exit 1
+}
+
+
+#
+# Purpose: Check command dependency
+#
+checkDependency()
+{
+    local cmd="$1"
+    local cmdName="$2"
+    
+    if [ -x "$(command -v $cmd)" ]; then
+        log "[X] $cmdName is installed..." success
+        return 0
+    else
+        log "[ ] $cmdName is not installed..." error
+        return 1
+    fi
+}
+
+#
+# Purpose: Check if logged user is root
+#
+checkIfIsRoot()
+{
+    #Root Login
+    if [ $(id -u) -eq 0 ]
+    then 
+        log "[X] Running as Root..." success
+        return 0
+    else
+        log "[ ] Not running as Root..." error
+        return 1
+    fi
+}
