b703cbecaad8b6e56d701ac19113ceeaa87510d4
Bruno Crema Ferreira <eng.brn@gmail.com>
2021-06-19 18:40:22 -0300

    Update scripts

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -77,5 +77,34 @@ installVsExtension "abusaidm.html-snippets"
 installVsExtension "ms-azuretools.vscode-docker"
 installVsExtension "fernandoescolar.vscode-solution-explorer"
 
+#------------------|Installing Docker  |------------------
+
+checkDependency docker "Docker"
+if [ $? -eq 1 ]; then
+    log "Installing..." success
+
+    #Configuring
+    sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    #Installing
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+    #Testing
+    sudo docker run hello-world
+    sudo docker ps -a
+    #sudo docker container rm hello-world
+
+fi
 #------------------|End of Script|------------------
 log "End of Script..." success
