9791421da90d3299af43cff92279f0e404b5dfc6
Bruno Crema Ferreira <eng.brn@gmail.com>
2021-07-04 17:56:14 -0300

    Code coverage report

---
+++ scripts/test-report.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+. ./lib.sh
+
+createXMLReport()
+{
+    log "Generating XML Test Report..." title
+    cd "../test/LiloDash.Tests"
+
+    log "Removing last report..." information
+    rm -v -R "TestResults"
+
+    log "Running tests..." information
+    dotnet test --collect:"XPlat Code Coverage" --settings CodeCoverage.runsettings
+}
+
+createHTMLReport()
+{
+    log "Creating HTML Coverage Report..." title
+    cd "TestResults"
+
+    local generatedDir=$(ls)
+    cd "$generatedDir"
+
+    log "PWD: $(pwd)" success
+
+    log "Running..." information
+    reportgenerator "-reports:coverage.coverage.xml" "-targetdir:coveragereport" -reporttypes:Html 
+}
+
+openReport()
+{
+    cd "coveragereport"
+    explorer index.html
+}
+
+main()
+{
+    clear
+    log "COVERAGE TEST REPORT" intro 1.0.0
+    createXMLReport
+    createHTMLReport
+    openReport
+}
+
+main
