6cf481f37f77c3e752e5cd892a06d9ded963f443
Bruno Crema Ferreira <eng.brn@gmail.com>
2021-07-06 20:45:55 -0300

    AppService Test

---
+++ test/LiloDash.Tests/0.2.0.Application/Services/BuildingAppServiceTest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using LiloDash.Domain.Model;
+using LiloDash.Infra.Data.Context;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using LiloDash.API.Controllers;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LiloDash.Application.ViewModels.Building;
+using LiloDash.Application.Interfaces.Services;
+using Bogus;
+
+namespace LiloDash.Tests.Application.Services
+{
+    public class BuildingAppServiceTest: Test<Building>
+    {
+        private readonly IBuildingAppService _buildingAppService;
+
+        public BuildingAppServiceTest(LiloDataContext context,
+            IBuildingAppService buildingAppService)
+            :base(context)
+        {
+            _buildingAppService = buildingAppService;
+        }
+
+        [Fact(DisplayName = "GetById - Should return Building")]
+        [Trait("Category", "BuildingAppService")]
+        public async Task Building_GetById_ShouldReturnBuildingById()
+        {
+            //Arrange
+            var building = await Context.Buildings.FirstOrDefaultAsync();
+
+            //Act
+            var result = await _buildingAppService.GetById(building.Id);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(result.Id, building.Id);
+        }
+
+        [Fact(DisplayName = "Add - Should return Building")]
+        [Trait("Category", "BuildingAppService")]
+        public async Task Building_Add_ShouldAddWithSuccess()
+        {
+            //Arrange
+            var buildingAddViewModel = BuildingAddViewModelGetNewInstance();
+
+            //Act
+            var result = await _buildingAppService.Add(buildingAddViewModel);
+
+            //Assert
+            Assert.True(result.Result.IsValid);
+            Assert.NotNull(result.Id);
+            Assert.NotEqual(result.Id, Guid.Empty);
+        }
+
+        protected async override void CreateDatabaseSeed()
+        {
+            //InMemory Database Mock
+            Context.Database.EnsureDeleted();
+            Context.Buildings.Add(new Building(Guid.NewGuid(), "House"));
+            await Context.Commit();
+        }
+
+        private BuildingAddViewModel BuildingAddViewModelGetNewInstance()
+            => new Faker<BuildingAddViewModel>().
+                CustomInstantiator(e=> new BuildingAddViewModel
+                {
+                    Name = e.Name.FirstName()
+                });
+    }
+}
