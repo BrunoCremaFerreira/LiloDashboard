9dffc125fa27570e9b4f46d04ed0dcd1433979ae
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-05-31 23:04:23 -0300

    k8s script

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -14,10 +14,42 @@ if [ $? -eq 1 ]; then
 fi 
 
 log "Setting KUBECONFIG" title
+
+if [ ! -f "$1" ]; then
+    die "The '$1' kubernetes configuration file was not found."
+fi
+
 log  "Setting environment variable with Kubernete cluster yaml config path '$1'"
 export KUBECONFIG="$1"
 
-log "" title
+cd ../k8s/01-App
+log "Configuring MySql Database on Kubernetes" title
+log "Applying deployment.yaml..." title
+kubectl apply -f msql/deployment.yaml
+log "Applying service.yaml..." title
+kubectl apply -f msql/service.yaml
+
+log "Configuring RabbitMq on Kubernetes" title
+log "Applying deployment.yaml..." title
+kubectl apply -f rabbit-mq/deployment.yaml
+log "Applying service.yaml..." title
+kubectl apply -f rabbit-mq/service.yaml
+
+log "Configuring LiloDash application on Kubernetes" title
+log "Applying config-map.yaml..." title
+kubectl apply -f web-api/config-map.yaml
+log "Applying deployment.yaml..." title
+kubectl apply -f web-api/deployment.yaml
+log "Applying service.yaml..." title
+kubectl apply -f web-api/service.yaml
+log "Applying service-load-balancer.yaml..." title
+kubectl apply -f web-api/service-load-balancer.yaml
+
+log "Kubernetes Get All" title
+kubectl get all
+
+
+
 
 
 
