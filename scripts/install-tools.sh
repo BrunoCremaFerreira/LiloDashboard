74816a7af41d207d6ab12d2d3c64f41fa105aa72
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-01 01:44:24 -0300

    Tools script update

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -90,6 +90,18 @@ installVsExtension "abusaidm.html-snippets"
 installVsExtension "ms-azuretools.vscode-docker"
 installVsExtension "fernandoescolar.vscode-solution-explorer"
 
+#-----------------|Installing Kube Lens|------------
+
+checkDependency dotnet-ef "lens-desktop"
+if [ $? -eq 1 ]; then
+    log "Installing..." success
+    curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
+    sudo apt update
+    sudo apt install lens
+    lens-desktop
+fi
+
 #------------------|Installing Docker  |------------------
 
 #
